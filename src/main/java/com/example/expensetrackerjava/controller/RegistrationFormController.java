3f22fa349ba37507c8ec5cc5356e4dfe81a908cc
Chris Holzheu <chrisholzheu@elranchomarket.com>
2023-04-27 11:26:28 -0700

    finishing rough draft of login form, adding style and logic to registration form fxml and css, updating registration controller

--- src/main/java/com/example/expensetrackerjava/controller/RegistrationFormController.java
+++ src/main/java/com/example/expensetrackerjava/controller/RegistrationFormController.java
@@ -1,4 +1,44 @@
 package com.example.expensetrackerjava.controller;
 
+import com.example.expensetrackerjava.repository.DatabaseConnection;
+import com.example.expensetrackerjava.repository.daos.UserDao;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class RegistrationFormController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private Button registrationButton;
+
+    private UserDao userDao;
+
+    public RegistrationFormController() {
+        userDao = new UserDao(DatabaseConnection.getInstance().getConnection());
+    }
+
+    @FXML
+    private void handleRegister(ActionEvent event) {
+
+    }
 }
